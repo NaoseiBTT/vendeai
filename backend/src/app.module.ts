6eb3d90445b80b2f986c23948dccfebf5b019946
ALUNO SENAI PETROLINA <aluno.petrolina@senai.pe>
2026-09-16 08:58:23 -0300

    Corrige TypeORM para ler variaveis de ambiente na nuvem

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,14 +12,14 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
   imports: [
 
     TypeOrmModule.forRoot({
-      type:"mysql",
-      host:"localhost",
-      port:3306,
-      username:"root",
-      password:"root",
-      database:"vendeai",
-      autoLoadEntities:true,
-      synchronize:true
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_NAME || 'vendeai',
+      autoLoadEntities: true,
+      synchronize: true,
     }),
 
     ProductsModule,
