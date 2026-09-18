9b878cd834afe72443e24f20e338a30ad6b8644a
ALUNO SENAI PETROLINA <aluno.petrolina@senai.pe>
2026-09-18 08:48:31 -0300

    Ci and Sonar Update

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  resolve: {
-    tsconfigPaths: true,
-  },
-  test: {
-    globals: true,
-    root: './',
-    include: ['**/*.spec.ts'],
-  },
+ resolve: {
+  tsconfigPaths: true,
+ },
+
+ test: {
+  globals: true,
+  root: './',
+  include: ['**/*.spec.ts'],
+
+  coverage: {
+   provider: 'v8',
+   reporter: ['text', 'html', 'lcov'],
+  },
+ },
 });
